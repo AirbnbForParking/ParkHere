0ed71c225259a26162520696158ed9a8497bb7da
Mike Hsieh <mythsieh@gmail.com>
2015-09-29 12:50:16 -0700

    Client side routing added

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,48 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    // sets up one state to be the default state of parent
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.history', {
+    url: '/history',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/history.html'
+      }
+    }
+  })
+
+  .state('app.searches', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/searches.html',
+        controller: 'SearchesCtrl'
+      }
+    }
+  })
+
+  .state('app.search', {
+    url: '/search/:searchId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html',
+        controller: 'SearchCtrl'
+      }
+    }
+  });
+
+  //Fallback Route
+  $urlRouterProvider.otherwise('/app/history');
+
+});
