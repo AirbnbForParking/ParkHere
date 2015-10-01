e8f4ea22771662a5616456091a17af0975c50f7f
Mike Hsieh <mythsieh@gmail.com>
2015-10-01 16:33:45 -0700

    Added clear markers functionality

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -3,7 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+// angular.module('starter', ['ionic', 'starter.controllers', 'ionic-timepicker'])
 angular.module('starter', ['ionic', 'starter.controllers'])
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -42,7 +42,20 @@ angular.module('starter.controllers', ['starter.services'])
     {id: 2, address: '1 World Way, Los Angeles, CA', seller: 'John', price: 15, lat: 33.94224, lng: -118.40279},
     {id: 3, address: '652 Polk St San Francisco, CA', seller: 'Katherine', price: 20, lat: 37.78291, lng: -122.41902},
     {id: 4, address: 'Dick\'s Sporting Goods Concord, NH 03301 United States', seller: 'Christina', price: 25, lat: 43.22697, lng: -71.48562},
-    {id: 5, address: 'Highland Middle School, 15027 Bel-Red Rd Bellevue, WA 98007, United States', seller: 'Bob', price: 30, lat: 47.62657, lng: -122.14020}
+    {id: 5, address: 'Highland Middle School, 15027 Bel-Red Rd Bellevue, WA 98007, United States', seller: 'Bob', price: 30, lat: 47.62657, lng: -122.14020},
+
+    // dummy cluster
+    {id: 6, address: '582 Sutter St San Francisco, CA 94102', seller: 'Bob', price: 30, lat: 37.78918, lng: -122.40993},
+    {id: 7, address: '678 Post St San Francisco, CA 94109', seller: 'Bob', price: 30, lat: 37.78776, lng: -122.41295},
+    {id: 8, address: '850 Bush St San Francisco, CA 94108', seller: 'Bob', price: 30, lat: 37.79003, lng: -122.41134},
+    {id: 9, address: '144 Taylor St San Francisco, CA 94102', seller: 'Bob', price: 30, lat: 37.78391, lng: -122.41067},
+    {id: 10, address: '912 Sutter St San Francisco, CA 94104', seller: 'Bob', price: 30, lat: 37.78854, lng: -122.41548},
+    {id: 11, address: '754 Post St San Francisco, CA 94109', seller: 'Bob', price: 30, lat: 37.78778, lng: -122.41436},
+    {id: 12, address: '670 Larkin St San Francisco, CA 94109', seller: 'Bob', price: 30, lat: 37.78407, lng: -122.41759},
+    {id: 13, address: '798 Sutter St San Francisco, CA 94109', seller: 'Bob', price: 30, lat: 37.78878, lng: -122.41347},
+    {id: 14, address: '871 Sutter St San Francisco, CA 94109', seller: 'Bob', price: 30, lat: 37.78823, lng: -122.41459},
+    {id: 15, address: '619 Taylor St San Francisco, CA 94102', seller: 'Bob', price: 30, lat: 37.78820, lng: -122.41204}
+
   ];
 
   // User lists address for rent
@@ -56,6 +69,7 @@ angular.module('starter.controllers', ['starter.services'])
     // Geocode the address searched for (not a listing address) and add marker at that address. 
 
   var map;
+  var markers = [];
 
   //Called when input is submitted
   $scope.getSearches = function(input){
@@ -70,12 +84,59 @@ angular.module('starter.controllers', ['starter.services'])
         center: location
       };
       map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      google.maps.event.addListener(map, 'idle', $scope.showMarkers);
+
+
       $scope.addMarkerGeo(location, map);
-      $scope.markers = $scope.searches.forEach(function(obj) {
-        $scope.addMarker(obj,map);
-      });
+      // $scope.searches.forEach(function(obj) {
+      //   markers.push($scope.addMarker(obj,map));
+      // });
     });
-  },
+  };
+
+  // Show Markers
+  $scope.showMarkers = function(){
+    console.log('I am idle!');
+    var bounds = map.getBounds();
+    console.log(markers);
+    console.log('this is bounds: ', bounds);
+    console.log('this is bounds.Ga: ', bounds.Ga); //Ka = lat, Ga = long
+    console.log('this is bounds.Ga.j: ', bounds.Ga.j); // js is the left boundary
+    console.log('this is bounds.Ga.H: ', bounds.Ga.H); // H is the right boundary
+    //bounds.Ka.j // lat, j is the top boundary
+    //bounds.Ka.H // lat, H is the bottom boundary
+    
+
+
+
+    // Call the server with ajax passing it the bounds  
+
+    // In the ajax callback delete the current markers and add new markers
+      // delete current markers
+        $scope.clearMarkers();
+      // this happens on the server
+        // filter markers
+        var filtered = searches.filter(function(parkingSpot){
+          if (parkingSpot)
+            // figure lower lat and higher lat bound
+            // figure lower long and higher long bound
+        })
+
+
+      // read current markers
+      // dummy data, replace with real later
+        $scope.searches.forEach(function(obj) {
+          markers.push($scope.addMarker(obj,map));
+        });
+  };
+
+  //Clear current markers
+  $scope.clearMarkers = function(){
+    for (var i = 0; i < markers.length; i++){
+      markers[i].setMap(null);
+      markers = [];
+    }
+  };
 
   // Add marker to map for the input to search bar
   $scope.addMarkerGeo = function(location, map) {
@@ -87,7 +148,7 @@ angular.module('starter.controllers', ['starter.services'])
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
     return marker;
-  },
+  };
 
   // Get latitude and longitude coords from Address
   $scope.AddressToLocation = function(addressString, callback) {
@@ -100,7 +161,7 @@ angular.module('starter.controllers', ['starter.services'])
         alert("Geocode was not successful for the following reason: " + status);
       }
     });
-  }
+  };
 
   // Add marker while iterating through array of database results
   $scope.addMarker = function(obj, map) {
@@ -110,14 +171,14 @@ angular.module('starter.controllers', ['starter.services'])
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
     return marker;
-  }
+  };
 
   $scope.addressArray = function(queryArray) {
     return _.map(queryArray, function(element){
       if (element.address !== undefined)
         return element.address;
     });
-  }
+  };
 
   //API call when ready
   // $scope.searches = Search.query();
@@ -166,13 +227,36 @@ angular.module('starter.controllers', ['starter.services'])
 
 })
 
-.controller('ListingCtrl', function($scope){
+// .controller('ListingCtrl', function($scope){
 
-  $scope.submitListing = function(listing){
-    console.log(listing);
-  }
+//   $scope.timePickerObject = {
+//     inputEpochTime: ((new Date()).getHours() * 60 * 60),  //Optional
+//     step: 1,  //Optional
+//     format: 12,  //Optional
+//     titleLabel: '12-hour Format',  //Optional
+//     setLabel: 'Set',  //Optional
+//     closeLabel: 'Close',  //Optional
+//     setButtonType: 'button-positive',  //Optional
+//     closeButtonType: 'button-stable',  //Optional
+//     callback: function (val) {    //Mandatory
+//       timePickerCallback(val);
+//     } 
+//   };
 
-});
+//   function timePickerCallback(val) {
+//     if (typeof (val) === 'undefined') {
+//       console.log('Time not selected');
+//     } else {
+//       var selectedTime = new Date(val * 1000);
+//       console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), ':', selectedTime.getUTCMinutes(), 'in UTC');
+//     }
+//   }
+
+//   $scope.submitListing = function(listing){
+//     console.log(listing);
+//   }
+
+// });
 
 
 
