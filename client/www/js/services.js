fc235f49b2afd2f4575ffb191164f9bb38bca658
myazhbin <myazhbin@gmail.com>
2015-10-05 08:41:25 +0800

    Trying to get login to work with server

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -16,18 +16,19 @@ angular.module('starter.services', ['ngResource'])
 })
 
 .factory('Login', function($http){
-  var signIn = function(loginData) {
-    return $http({
-      method: 'POST',
-      url: '/api/login',
-      data: loginData
-    })
-    .then(function(response){
-      return response.data.token;
-      console.log(response);
-    });
+  return {
+    signIn: function(loginData) {
+      return $http({
+        method: 'POST',
+        url: '/api/login',
+        data: loginData
+      })
+      .then(function(response){
+        return response.data.token;
+        console.log('response: ',response);
+      });
+    }
   };
-  signIn(loginData);
 })
 
 .factory('Register', function($http){
@@ -45,8 +46,32 @@ angular.module('starter.services', ['ngResource'])
   registerIn(registerData);
 })
 
-.factory('Listing', function($resource){
-  return $resource('http://localhost:5000/listing/:listingId');
+.factory('Listing', function($http){
+  var getListings = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/listing'
+    })
+    .then(function(response){
+      return response.data;
+      console.log(response.data);
+    });
+  };
+
+  var addListing = function(listing) {
+    return $http({
+      method: 'POST',
+      url: '/api/listing',
+      data: listing
+    })
+    .then(function(response){
+      return response.data.token;
+      console.log(response);
+    });
+  };
+
+  getListings();
+
 })
 
 .factory('Profile', function($resource){
